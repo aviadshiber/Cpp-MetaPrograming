c80177eb3379c714509ef5e70c6db17fbd5bf3f5
aviadshiber <aviadshiber@campus.technion.ac.il>
2017-06-25 18:17:44 +0300

    implementing IntList

--- OOP5_SOL/OOP5_SOL/p1/IntList.h
+++ OOP5_SOL/OOP5_SOL/p1/IntList.h
@@ -4,5 +4,19 @@
 template<int...>
 struct IntList;
 
+template<int H,int...TT>
+struct IntList<H,TT...> {
+	constexpr static int head = H;
+	typedef IntList<TT...> next; //unpacking to be T, TT...T
+	constexpr static int size = next::size + 1;
+	constexpr static bool empty = false;
+};
+
+
+template<>
+struct IntList<> { //empty list
+	constexpr static bool empty = true;
+	constexpr static int size = 0;
+};
 
 #endif
